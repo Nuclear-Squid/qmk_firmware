524a5855199b1fd4aa06224e65179c6a94802457
Nuclear Squid <leo@cazenave.cc>
2024-06-30 19:40:19 +0200

    feat(ploopy): mouse acceleration.

--- keyboards/ploopyco/trackball_nano/keymaps/custom/keymap.c
+++ keyboards/ploopyco/trackball_nano/keymaps/custom/keymap.c
@@ -1,7 +1,8 @@
 #include QMK_KEYBOARD_H
+#include <math.h>
 
-#define DELTA_X_THRESHOLD 60
-#define DELTA_Y_THRESHOLD 15
+#define DELTA_X_THRESHOLD 50
+#define DELTA_Y_THRESHOLD 25
 
 // safe range starts at `PLOOPY_SAFE_RANGE` instead.
 
@@ -9,27 +10,75 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {{{ KC_NO }}};
 
 static bool num_lock_state = false;
 
-static int8_t delta_x = 0;
-static int8_t delta_y = 0;
-
 void keyboard_post_init_user(void) {
     num_lock_state  = host_keyboard_led_state().num_lock;
 }
 
+void handle_mouse_acceleration(report_mouse_t* mouse_report) {
+    #define NB_DISTANCE_VALUES 100
+    static float distance_values[NB_DISTANCE_VALUES] = {};
+    static float total_distance = 0;
+    static size_t distance_index = 0;
+
+    // cast to u16 to prevent integer overflow when squaring the value.
+    const int16_t mouse_x = mouse_report->x;
+    const int16_t mouse_y = mouse_report->y;
+
+    const float old_distance = distance_values[distance_index];
+    const float new_distance = sqrt(mouse_x * mouse_x + mouse_y * mouse_y);
+
+    total_distance -= old_distance;
+    total_distance += distance_values[distance_index] = new_distance;
+
+    distance_index = (distance_index + 1) % NB_DISTANCE_VALUES;
+
+    const float gain = 1.5;
+    const float accel = 1 + total_distance * gain / NB_DISTANCE_VALUES;
+    // const uint16_t base_dpi = 200;
+    // const uint16_t max_dpi = 1000;
+
+    // pointing_device_set_cpi(MIN((uint16_t) base_dpi * accel, max_dpi));
+
+    mouse_report->x *= MIN(accel, 5.);
+    mouse_report->y *= MIN(accel, 5.);
+}
+
+#define ABS(val) (val < 0 ? -val : val)
+#define CLAMP(val, min, max) (MIN(MAX(val, min), max))
+
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    handle_mouse_acceleration(&mouse_report);
+
+    static int8_t delta_x = 0;
+    static int8_t delta_y = 0;
     static uint16_t roll_timer = 0;
-    static int8_t roll_delta_x = 0;
-    static int8_t roll_delta_y = 0;
     static bool scroll_lock_on = false;
 
-    roll_delta_x += mouse_report.x;
-    roll_delta_y += mouse_report.y;
+    delta_x += mouse_report.x;
+    delta_y += mouse_report.y;
 
-    if (roll_delta_x > DELTA_X_THRESHOLD || delta_x < -DELTA_X_THRESHOLD ||
-        roll_delta_y > DELTA_Y_THRESHOLD || delta_y < -DELTA_Y_THRESHOLD
+    if (num_lock_state) {
+        mouse_report.x = 0;
+        mouse_report.y = 0;
+
+        if (ABS(delta_x) > DELTA_X_THRESHOLD) {
+            mouse_report.h = delta_x / DELTA_X_THRESHOLD;
+            delta_x %= DELTA_X_THRESHOLD;
+        }
+
+        if (ABS(delta_y) > DELTA_Y_THRESHOLD) {
+            mouse_report.v = -delta_y / DELTA_Y_THRESHOLD;
+            delta_y %= DELTA_Y_THRESHOLD;
+        }
+
+        return mouse_report;
+    }
+
+    if (delta_x > DELTA_X_THRESHOLD || delta_x < -DELTA_X_THRESHOLD ||
+        delta_y > DELTA_Y_THRESHOLD || delta_y < -DELTA_Y_THRESHOLD
     ) {
         roll_timer = timer_read();
-        roll_delta_x = roll_delta_y = 0;
+        delta_x = delta_y = 0;
         if (!scroll_lock_on) {
             scroll_lock_on = true;
             tap_code(KC_SCROLL_LOCK);
@@ -40,28 +89,6 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
         tap_code(KC_SCROLL_LOCK);
     }
 
-    if (num_lock_state) {
-        delta_x += mouse_report.x;
-        delta_y += mouse_report.y;
-
-        if (delta_x > DELTA_X_THRESHOLD) {
-            mouse_report.h = 1;
-            delta_x        = 0;
-        } else if (delta_x < -DELTA_X_THRESHOLD) {
-            mouse_report.h = -1;
-            delta_x        = 0;
-        }
-
-        if (delta_y > DELTA_Y_THRESHOLD) {
-            mouse_report.v = -1;
-            delta_y        = 0;
-        } else if (delta_y < -DELTA_Y_THRESHOLD) {
-            mouse_report.v = 1;
-            delta_y        = 0;
-        }
-        mouse_report.x = 0;
-        mouse_report.y = 0;
-    }
     return mouse_report;
 }
 
